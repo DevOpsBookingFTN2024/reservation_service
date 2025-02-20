b9d4c5f2aed1deda215065311a40fd955c8a35a1
Nikola Jovic <jovnikola99@gmail.com>
2025-02-20 13:35:13 +0100

    feat: Add Interceptor to Track Unique Visitors Metric (#36)

---
+++ src/main/java/uns/ac/rs/reservation_service/config/WebConfig.java
@@ -0,0 +1,22 @@
+package uns.ac.rs.reservation_service.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import uns.ac.rs.reservation_service.metrics.UniqueVisitorInterceptor;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private final UniqueVisitorInterceptor uniqueVisitorInterceptor;
+
+    @Autowired
+    public WebConfig(UniqueVisitorInterceptor uniqueVisitorInterceptor) {
+        this.uniqueVisitorInterceptor = uniqueVisitorInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(uniqueVisitorInterceptor);
+    }
+}
